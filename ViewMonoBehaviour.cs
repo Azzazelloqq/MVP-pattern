62ea9ff82eb1883428f2a086fb6c24bbf83865d1
d.bogdanov <d.bogdanov@nexters.com>
2024-09-21 14:05:32 +0400

    change unity view for new base class

--- ViewMonoBehaviour.cs
+++ ViewMonoBehaviour.cs
@@ -1,32 +1,14 @@
 ﻿using Disposable;
-using UnityEngine;
 
 namespace MVP
 {
-public abstract class ViewMonoBehaviour<TPresenter> : MonoBehaviour, IView
-    where TPresenter : IPresenter
-{
-    protected TPresenter presenter;
-    protected bool isDisposed;
-    protected readonly ICompositeDisposable compositeDisposable = new CompositeDisposable();
+	public abstract class ViewMonoBehaviour<TPresenter> : MonoBehaviourDisposable, IView where TPresenter : IPresenter
+	{
+		protected TPresenter presenter;
 
-    public void Initialize(TPresenter presenter)
-    {
-        this.presenter = presenter;
-    }
-
-    public void Dispose()
-    {
-        if (isDisposed)
-        {
-            return;
-        }
-        
-        compositeDisposable?.Dispose();
-        
-        Destroy(gameObject);
-
-        isDisposed = true;
-    }
-}
+		public void Initialize(TPresenter presenter)
+		{
+			this.presenter = presenter;
+		}
+	}
 }
